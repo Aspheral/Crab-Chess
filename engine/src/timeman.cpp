8a76716181c21c45c5cf975c710b925298dd7972
Aspheral <aspheralmusic@gmail.com>
2026-09-08 06:08:04 -0500

    EXP-0027: preserve cyclic movestogo horizon

--- engine/src/timeman.cpp
+++ engine/src/timeman.cpp
@@ -89,8 +89,9 @@ void TimeManagement::init(Search::LimitsType& limits,
     // Maximum move horizon
     int centiMTG = limits.movestogo ? std::min(limits.movestogo * 100, 5000) : 5051;
 
-    // If less than one second, gradually reduce mtg
-    if (scaledTime < 1000)
+    // If less than one second, gradually reduce mtg.
+    // In cyclic time controls keep the explicit movestogo horizon.
+    if (scaledTime < 1000 && limits.movestogo == 0)
         centiMTG = int(scaledTime * 5.051);
 
     // Make sure timeLeft is > 0 since we may use it as a divisor
